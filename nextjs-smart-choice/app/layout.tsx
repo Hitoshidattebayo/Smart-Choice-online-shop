2781e3827ebdee96d687a4bd0e20a2fc3ce658fd
Hitoshidattebayo <your-email@example.com>
2026-03-08 05:04:26 +0800

    refactor: extract Meta Pixel logic into a FacebookPixel component with Suspense wrapper

--- nextjs-smart-choice/app/layout.tsx
+++ nextjs-smart-choice/app/layout.tsx
@@ -6,6 +6,7 @@ import { CartProvider } from "../context/CartContext";
 import { Providers } from "../components/Providers";
 import { DevIndicator } from "../components/DevIndicator";
 import DraggableTurboToast from "../components/DraggableTurboToast";
+import FacebookPixel from "../components/meta/FacebookPixel";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -27,41 +28,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                {/* Meta Pixel Code */}
-                {process.env.META_PIXEL_ID && (
-                    <Script
-                        id="fb-pixel"
-                        strategy="afterInteractive"
-                        dangerouslySetInnerHTML={{
-                            __html: `
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '${process.env.META_PIXEL_ID}');
-fbq('track', 'PageView');
-                            `,
-                        }}
-                    />
-                )}
-                {/* End Meta Pixel Code */}
             </head>
             <body>
-                {process.env.META_PIXEL_ID && (
-                    <noscript>
-                        <img
-                            height="1"
-                            width="1"
-                            style={{ display: "none" }}
-                            src={`https://www.facebook.com/tr?id=${process.env.META_PIXEL_ID}&ev=PageView&noscript=1`}
-                            alt=""
-                        />
-                    </noscript>
-                )}
+                <FacebookPixel />
                 <Providers>
                     <CartProvider>
                         <DevIndicator />
